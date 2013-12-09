acb2ea006fc7cbe1c81fe75040b034a5b5666c76
Rahat Ali <rahat.jaan@gmail.com>
2013-12-09 16:21:56 +0500

    Model

---
+++ NotificationWS/src/main/java/com/servercentral/communication/hub/model/Attachment.java
@@ -0,0 +1,17 @@
+package com.servercentral.communication.hub.model;
+
+public class Attachment {
+
+	Byte[] attachment;
+	String contentType;
+	
+
+	public Byte[] getAttachment() {
+		return attachment;
+	}
+
+	public void setAttachment(Byte[] attachment) {
+		this.attachment = attachment;
+	}
+	
+}
---
+++ NotificationWS/src/main/java/com/servercentral/communication/hub/model/Notification.java
@@ -0,0 +1,50 @@
+package com.servercentral.communication.hub.model;
+
+public class Notification {
+
+	String subject;
+	String from;
+	String recipientGuid;
+	String to;
+	String cc;
+	String messageBody;
+	Attachment[] attachments;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getCc() {
+		return cc;
+	}
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+	public String getMessageBody() {
+		return messageBody;
+	}
+	public void setMessageBody(String messageBody) {
+		this.messageBody = messageBody;
+	}
+	public Attachment[] getAttachments() {
+		return attachments;
+	}
+	public void setAttachments(Attachment[] attachments) {
+		this.attachments = attachments;
+	}
+	
+}
