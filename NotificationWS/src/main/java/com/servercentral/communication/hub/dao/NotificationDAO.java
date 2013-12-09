79219330976a7399e3535b69481d2ccc0f8d42b6
Rahat Ali <rahat.jaan@gmail.com>
2013-12-09 18:01:53 +0500

    notification dao

--- NotificationWS/src/main/java/com/servercentral/communication/hub/dao/NotificationDAO.java
+++ NotificationWS/src/main/java/com/servercentral/communication/hub/dao/NotificationDAO.java
@@ -2,12 +2,17 @@ package com.servercentral.communication.hub.dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.servercentral.communication.hub.model.Notification;
 
 public class NotificationDAO {
 
-	public boolean saveNotification() throws SQLException{
+	public boolean saveNotification(Notification notification) throws SQLException{
 		Connection conn = ConnectionUtil.getConnection();
-		conn.createStatement();
+		Statement stmt= conn.createStatement();
+		String sql = "";
+		stmt.executeUpdate(sql);
 		return true;
 		
 	}
